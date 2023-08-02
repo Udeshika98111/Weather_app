4d98f0c1e029b8087244dc5100d20b8ff6807eb4
Udeshika Sethunga <smsusethunga@gmail.com>
2023-08-02 22:03:47 +0530

    1st Commit

---
+++ Assest/Assest/Pages/model/LoginModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assest.Pages.model
+{
+    public class LoginModel
+    {
+        public object ViewData { get; private set; }
+
+        public void OnGet()
+        {
+          
+        }
+
+        public IActionResult OnPost(string username, string password)
+        {
+           
+            if (username == "Micky" && password == "123")
+            {
+              
+                return RedirectToPage("/login");
+            }
+            else
+            {
+                
+                 
+                //ViewData ["ErrorMessage"] = "Invalid username or password.";
+                return Page();
+            }
+        }
+
+        private IActionResult Page()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IActionResult RedirectToPage(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
